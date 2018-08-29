a17006660e2573fee525968ce1437a898194c0ea
Fabrício Lago <fabricio@hipcom.com.br>
2018-08-29 16:25:14 -0300

    novo projeto node

---
+++ src/app.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const ConversationV1 = require('watson-developer-cloud/conversation/v1');
+
+const app = express();
+const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+const route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: "Node Store API",
+        version: "0.0.1"
+    });
+});
+
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+
+// const put = router.put('/:id1/:id2', (req, res, next) => {
+//     const id1 = req.params.id1;
+//     const id2 = req.params.id2;
+//     res.status(201).send({
+//         id: id1,
+//         item: req.body
+//     });
+// });
+
+const put = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(201).send({
+        id: id,
+        item: req.body
+    });
+});
+
+app.use('/', route);
+app.use('/products', create);
+app.use('/products', put);
+
+module.exports = app;
